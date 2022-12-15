4e62cefef4f12baa990f9c4724933ad4e56e165d
jhoon9494 <wjdgns9406@gmail.com>
2022-12-15 17:13:52 +0900

    Style: 다크모드 배경색상 수정

--- styles/GlobalStyle.tsx
+++ styles/GlobalStyle.tsx
@@ -4,9 +4,6 @@ const GlobalStyle = createGlobalStyle<{ themeColor: string | false | null }>`
   *, *::before, *::after {
     box-sizing: border-box;
   }
-  html{
-    color-scheme: ${({ themeColor }) => (themeColor === 'dark' ? 'dark' : 'light')} 
-  }
 
   html,
   body {
@@ -16,10 +13,17 @@ const GlobalStyle = createGlobalStyle<{ themeColor: string | false | null }>`
       Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
   }
 
+
+  html{
+    color-scheme: ${({ themeColor }) => (themeColor === 'dark' ? 'dark' : 'light')};
+    background-color: ${({ themeColor }) => (themeColor === 'dark' ? '#222' : 'white')};
+  }
+  
   a {
     color: inherit;
     text-decoration: none;
   }
+
 `;
 
 export default GlobalStyle;
