6049865ae32b80ab7c860c07fa6c1cb521e48286
jhoon9494 <wjdgns9406@gmail.com>
2023-05-17 17:24:15 +0900

    Refactor: theme 관련 코드 Layout 컴포넌트로 이동
    app dir 하위의 layout.tsx를 서버 컴포넌트로 사용하고 Layout 컴포넌트만 클라이언트 컴포넌트로 사용하도록 변경

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,26 +1,13 @@
-'use client';
-
-import { ThemeProvider } from 'styled-components';
 import Layout from '../components/Layout';
-import useTheme from '../hooks/useTheme';
 import StyledComponentsRegistry from '../lib/registry';
-import GlobalStyle from '../styles/GlobalStyle';
-import { lightTheme, darkTheme } from '../styles/theme';
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
-  const { theme, setTheme } = useTheme();
-
   return (
     <html lang="ko">
       <head />
       <body>
         <StyledComponentsRegistry>
-          <ThemeProvider theme={theme === 'dark' ? darkTheme : lightTheme}>
-            <GlobalStyle />
-            <Layout theme={theme} setTheme={setTheme}>
-              {children}
-            </Layout>
-          </ThemeProvider>
+          <Layout>{children}</Layout>
         </StyledComponentsRegistry>
       </body>
     </html>
